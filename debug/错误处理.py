7c85ec044e55cef05e99465a10713f4adf61db87
wangzhen <wang765256860@gmail.com>
2018-07-10 12:12:11 +0800

    deal with error

---
+++ debug/错误处理.py
@@ -0,0 +1,33 @@
+"""
+def foo(s):
+    n = int(s)
+    if n==0:
+        raise ValueError('invalid value: %s' % s)
+    return 10 / n
+
+def bar():
+    try:
+        foo('0')
+    except ValueError as e:
+        print('ValueError!')
+        raise
+
+bar()
+"""
+from functools import reduce
+
+def str2num(s):
+    return float(s)
+
+def calc(exp):
+    ss = exp.split('+')
+    ns = map(str2num, ss)
+    return reduce(lambda acc, x: acc + x, ns)
+
+def main():
+    r = calc('100 + 200 + 345')
+    print('100 + 200 + 345 =', r)
+    r = calc('99 + 88 + 7.6')
+    print('99 + 88 + 7.6 =', r)
+
+main()
